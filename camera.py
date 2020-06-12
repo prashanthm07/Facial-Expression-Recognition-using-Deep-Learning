b70546e587e040370a4c11eef732f2a0d0ccc151
Mangipudi Prashanth Sarma <mangipudiprashanth10@gmail.com>
2020-06-12 17:14:39 +0530

    added requirements.txt file

--- camera.py
+++ camera.py
@@ -15,8 +15,8 @@ class VideoCamera(object):
 
     # returns camera frames along with bounding boxes and predictions
     def get_frame(self):
-        _, fr = self.video.read()
-        gray_fr = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
+        _, frame = self.video.read()
+        gray_fr = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = facec.detectMultiScale(gray_fr, 1.3, 5)
 
         for (x, y, w, h) in faces:
@@ -25,8 +25,8 @@ class VideoCamera(object):
             roi = cv2.resize(fc, (48, 48))
             pred = model.predict_emotion(roi[np.newaxis, :, :, np.newaxis])
 
-            cv2.putText(fr, pred, (x, y), font, 1, (255, 255, 0), 2)
-            cv2.rectangle(fr,(x,y),(x+w,y+h),(255,0,0),2)
+            cv2.putText(frame, pred, (x, y), font, 1, (255, 255, 0), 2)
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
 
-        _, jpeg = cv2.imencode('.jpg', fr)
+        _, jpeg = cv2.imencode('.jpg', frame)
         return jpeg.tobytes()
